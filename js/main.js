8c1a92eac7a539a23d449cca0bfb68ad57a0fbeb
rishabhag18 <18r.agarwal@gmail.com>
2024-08-19 12:04:29 +0530

    base project

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript to handle tree selection, etc.
+    // You can expand this based on your needs
+    console.log("Tree Plantation Service is ready!");
+});
